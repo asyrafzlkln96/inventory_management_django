2ee9bf267a4cbddee12a8c4e755280e9b52931cb
MOHD ASYRAF BIN ZULKALNAIN <53460015+asyrafzlkln96@users.noreply.github.com>
2022-10-24 01:11:03 +0800

    Update settings.py

--- inventorySystem/settings.py
+++ inventorySystem/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_DIRS = (
+    os.path.join('static'),
+)
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
