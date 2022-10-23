a291c3a7c73b9378c5c8119626ad037097ffcfda
MOHD ASYRAF BIN ZULKALNAIN <53460015+asyrafzlkln96@users.noreply.github.com>
2022-10-24 01:48:38 +0800

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 # from .views import index
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'inventory'
 
 urlpatterns = [
 	path("inventory", views.index, name="get_all_inventory_list"),
 	path("inventory/<int:inventory_id>", views.get_inventory_by_id, name='get_inventory_by_id'),
-	path("api/inventory", views.get_all_inventories, name='get_all_inventories'),
-	path("api/inventory/<str:name>", views.get_filtered_inventories, name='get_filtered_inventories')
+	path("api/inventory/", views.get_all_inventories, name='get_all_inventories')
 ]
